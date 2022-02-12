2c6dd8352dcb36c631ca1b25e5d8ec979038cd11
Sandeep Nagendra <sandeep.ee.nagendra@gmail.com>
2022-02-12 23:05:40 +0530

    This program find the last index of given element in an array

---
+++ last_index_of_element.cc
@@ -0,0 +1,32 @@
+/*
+Given an array find the last occurence of the element and return it's index.
+Problem is solved using recursion.
+
+*/
+#include <iostream>
+
+int find_last_index_of_an_element(int *arr, int size, int element)
+{
+    if(size == 0)
+        return -1;
+
+    if(arr[size - 1] == element)
+        return size -1;
+
+    return find_last_index_of_an_element(arr, size-1, element);
+}
+
+int main()
+{
+    int size;
+    std::cin >> size;
+    int *arr = new int[size];
+    for(int i = 0; i < size; i++)
+        std::cin >> arr[i];
+
+    int element;
+    std::cin >> element;
+    std::cout << find_last_index_of_an_element(arr, size, element);
+    std::cout << std::endl;
+    return 0;
+}
