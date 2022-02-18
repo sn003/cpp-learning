59d7a9a0033f149c5babf83744fef6ae1f4222cb
Sandeep Nagendra <sandeep.ee.nagendra@gmail.com>
2022-02-18 06:46:54 +0530

    print steps taken to explain tower of hanoi

---
+++ recursion/tower_of_hanoi_steps.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+void tower_of_hanoi_steps(std::string source, std::string helper, std::string destination, int disks)
+{
+    if(disks == 0)
+        return;
+
+    tower_of_hanoi_steps(source, destination, helper, disks-1);
+    std::cout << "Moving disk from  " << source << " to " << destination << std::endl;
+    tower_of_hanoi_steps(helper, source, destination, disks-1);
+
+
+}
+
+int main()
+{
+    int disks = 0;
+    std::cin >> disks;
+    tower_of_hanoi_steps("S", "H", "D", disks);
+    return 0;
+}
